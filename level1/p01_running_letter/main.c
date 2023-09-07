4b5b27b57903388a685f02807ce3f1443eb4559b
Mark Feng <luckyzhigang@gmail.com>
2023-09-07 13:39:41 +0800

    调整了作业、readme、cmakelist

---
+++ level1/p01_running_letter/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <windows.h>
+
+#define RIGHT 0
+#define LEFT  1
+
+HANDLE hd;
+int width,dir=RIGHT;
+void move(int x,int y)
+{
+    COORD pos={x,y};
+    SetConsoleCursorPosition(hd,pos);
+    return;
+}
+void GetSize()
+{
+    CONSOLE_SCREEN_BUFFER_INFO info;
+    GetConsoleScreenBufferInfo(hd,&info);
+    width=info.srWindow.Right;
+    return;
+}
+int ne(int now)
+{
+    if(dir==RIGHT)
+    {
+        if(now!=width) return now+1;
+        else
+        {
+            dir=LEFT;
+            return now-1;
+        }
+    }
+    else
+    {
+        if(now) return now-1;
+        else
+        {
+            dir=RIGHT;
+            return now+1;
+        }
+    }
+}
+
+int main() {
+    int now=0;
+    hd= GetStdHandle(STD_OUTPUT_HANDLE);
+    GetSize();
+    printf("a");
+    while(1)
+    {
+        move(now,0);
+        printf(" ");
+        now=ne(now);
+        move(now,0);
+        printf("a");
+        Sleep(25);
+    }
+    return 0;
+}
