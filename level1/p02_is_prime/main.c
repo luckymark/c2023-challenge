7a69d7a6b60a5534e4bfcabdc9b52cc29b33166d
TwilightLemon <cz241126@live.com>
2023-09-09 22:05:52 +0800

    level0 finished(except C3.5)

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,15 @@
-#include <printf.h>
+#include <stdio.h>
 
 int main() {
-    printf("hello world!\n");
+    //主要是为了无限测试
+    for(;;) {
+        printf("Enter a num:");
+        int n;
+        scanf("%d", &n);
+        int count = 0;
+        for (int i = 1; i < n; i++)
+            count += n % i == 0;
+        printf("%s", count==1 ? "is prime!\r\n" : "no\r\n");
+    }
     return 0;
 }
--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,20 @@
-#include <printf.h>
+#include <stdio.h>
+#include <time.h>
 
+int isPrime(int n){
+    int count = 0;
+    for (int i = 1; i < n; i++)
+        count += n % i == 0;
+    return count==1;
+}
 int main() {
-    printf("hello world!\n");
+    time_t start,done;
+    start=clock();
+    for(int i=2;i<=1000;i++){
+        if(isPrime(i))
+            printf("%d ",i);
+    }
+    done=clock();
+    printf("\r\nFinished in %f ms",(double)(done-start));
     return 0;
 }
