fee3dca01f140686ef0254c3ef8792080771a8ca
tengkaize <tengkaize@qq.com>
2023-09-09 20:43:46 +0800

    完成 level1 p03

--- level1/p03_all_primes/main.cpp
+++ level1/p03_all_primes/main.cpp
@@ -1,6 +1,35 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+// O(n)
+std::vector<unsigned> sieve(unsigned n) {
+	std::vector<bool> v(n + 1);
+	std::vector<unsigned> ps;
+
+	for (unsigned i = 2; i <= n; ++i) {
+		if (!v[i]) ps.push_back(i);
+		for (unsigned p : ps) {
+			if (i * p > n) break;
+			v[i * p] = true;
+			if (i % p == 0) break;
+		}
+	}
+
+	return ps;
+}
 
 int main() {
-    printf("hello world!\n");
-    return 0;
+	using namespace std;
+	using namespace chrono;
+
+	auto start = chrono::steady_clock::now();
+	auto primes = sieve(1000);
+	auto end = chrono::steady_clock::now();
+
+	for (unsigned p : primes) cout << p << ' ';
+	cout << endl;
+	cout << "Time cost for the calculation: " << (end - start) << endl;
+
+	return 0;
 }
