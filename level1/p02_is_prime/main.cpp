d8a018ee6d15f2e588392de4472dd457e4ab5cf5
tengkaize <tengkaize@qq.com>
2023-09-09 20:29:18 +0800

    完成 level1 p02

--- level1/p02_is_prime/main.cpp
+++ level1/p02_is_prime/main.cpp
@@ -1,6 +1,18 @@
-#include <cstdio>
+#include <iostream>
+
+bool is_prime(unsigned n) {
+	if (n == 0 || n == 1) return false;
+	for (unsigned i = 2; i * i <= n; ++i)
+		if (n % i == 0) return false;
+	return true;
+}
 
 int main() {
-    printf("hello world!\n");
-    return 0;
+	using namespace std;
+
+	unsigned n;
+	cin >> n;
+	cout << n << ' ' << (is_prime(n) ? "is" : "is not") << ' ' << "prime." << endl;
+
+	return 0;
 }
