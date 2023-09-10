9a4ff93e84474b09beac79cc548aac1d4752b7eb
YangQingc <yqc2213316205@outlook.com>
2023-09-11 00:45:39 +0800

    调整了作业、readme、cmakelist

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,22 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n,end;
+    scanf("%d",&n);
+    end=sqrt(n)+1;
+    if(n==1) printf("No");
+    else if(n==2) printf("Yes");
+    else
+    {
+        for(int i=2;i<=end;++i)
+        {
+            if(n%i==0) {
+                printf("No");
+                break;
+            }
+        }
+        printf("Yes");
+    }
     return 0;
 }
