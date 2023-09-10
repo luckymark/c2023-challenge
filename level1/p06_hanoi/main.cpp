314885ac29f85611a43149f01646be85db8ecb3c
tengkaize <tengkaize@qq.com>
2023-09-10 16:22:10 +0800

    完成 level1 p06

--- level1/p06_hanoi/main.cpp
+++ level1/p06_hanoi/main.cpp
@@ -1,6 +1,20 @@
 #include <cstdio>
 
+using namespace std;
+
+void hanoi(int n, char a, char b, char c) {
+	if (n == 1) {
+		printf("%c -> %c\n", a, c);
+		return;
+	}
+	hanoi(n - 1, a, c, b);
+	printf("%c -> %c\n", a, c);
+	hanoi(n - 1, b, a, c);
+}
+
 int main() {
-    printf("hello world!\n");
-    return 0;
+	int n;
+	scanf("%d", &n);
+	hanoi(n, 'A', 'B', 'C');
+	return 0;
 }
