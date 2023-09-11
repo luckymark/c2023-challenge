6742764509dde6d67a898d57646d7f15d3354523
tengkaize <tengkaize@qq.com>
2023-09-11 21:23:33 +0800

    完成 level1 p01

--- level1/p01_running_letter/main.cpp
+++ level1/p01_running_letter/main.cpp
@@ -1,6 +1,25 @@
 #include <cstdio>
+#include <thread>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
 int main() {
-    printf("hello world!\n");
-    return 0;
+	using namespace std;
+	using namespace std::chrono_literals;
+	winsize s;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &s);
+	int w = s.ws_col;
+	auto print_frame = [&](int c) {
+		for (int i = 0; i < w; ++i) {
+			if (i == c) putchar('*');
+			else putchar(' ');
+		}
+		putchar('\r');
+		std::this_thread::sleep_for(50ms);
+	};
+	while (true) {
+		for (int i = 0; i < w; ++i) print_frame(i);
+		for (int i = w; i-- > 0; ) print_frame(i);
+	}
+	return 0;
 }
