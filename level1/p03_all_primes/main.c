26deddee7979a87e6e896fb8c0d40359bc083fc0
YangQingc <yqc2213316205@outlook.com>
2023-09-11 09:13:46 +0800

    调整了作业、readme、cmakelist

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,25 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+#include <windows.h>
+const int N=1000;
+int NotPrime[N+1]={},primes[N+1],num=0;
+int main()
+{
+    WORD timer=0;
+    SYSTEMTIME tm0,tm1;
+    GetSystemTime(&tm0);
+    for(int i=2;i<=N;++i)
+    {
+        if(!NotPrime[i]) primes[num++]=i;
+        for(int j=0;j<num;++j)
+        {
+            int tem=primes[j]*i;
+            if(tem<=N) NotPrime[tem]=1;
+            if(i%primes[j]==0) break;
+        }
+    }
+    GetSystemTime(&tm1);
+    timer=(tm1.wSecond-tm0.wSecond)*1000+tm1.wMilliseconds-tm0.wMilliseconds;
+    printf("Running Time:%dms\n",timer);
+    for(int i=0;i<num;++i) printf("%d\n",primes[i]);
     return 0;
 }
