041dd1477ab9a5904ee4dce4b2e831a1c03ec55a
YangQingc <yqc2213316205@outlook.com>
2023-09-11 09:40:31 +0800

    调整了作业、readme、cmakelist

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,6 +1,18 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+void move(int cap,int tar,int buffer,int num)
+{
+    if(num==1)
+    {
+        printf("%c->%c\n",cap-1+'A',tar-1+'A');
+        return;
+    }
+    move(cap,buffer,tar,num-1);
+    printf("%c->%c\n",cap-1+'A',tar-1+'A');
+    move(buffer,tar,cap,num-1);
+    return;
+}
+int main()
+{
+    move(1,2,3,64);
     return 0;
 }
