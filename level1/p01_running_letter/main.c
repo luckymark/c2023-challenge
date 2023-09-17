3c1384498da835a1b8edad573a58e509ce13ac26
12ded21 <1421242252@qq.com>
2023-09-17 15:53:39 +0800

    作业

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,25 @@
-#include <printf.h>
-
+#include <stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<conio.h>
 int main() {
-    printf("hello world!\n");
+    char * letter="Hello";
+    int m=0;
+    int k=1;
+    while(1){
+        if(m==0)
+            k=1;
+        if(m==57)
+            k=-1;
+        for(int i=1;i<=m*2;i++)
+            printf(" ");
+        printf("%s",letter);
+        Sleep(300);
+        if(k==1)
+            m++;
+        else
+            m--;
+       system("cls");
+       }
     return 0;
 }
