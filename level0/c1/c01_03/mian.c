3731603fa43c6e2a7c48b1c5eb755357aaeae5ed
challenge_zzq_firstpush <812014285@qq.com>
2023-09-18 09:03:58 +0800

    补充提交，上一次遗漏

---
+++ level0/c1/c01_03/mian.c
@@ -0,0 +1,16 @@
+//判断闰年
+#include <stdio.h>
+
+void leap(int a){
+    if(a%4==0&&a&100!=0)
+        printf("您输入的是闰年");
+    else
+        printf("您输入的不是闰年");
+}
+int main(){
+    system("chcp 65001");
+    int a;
+    printf("请输入一个整数：");
+    scanf("%d",&a);
+    leap(a);
+}
