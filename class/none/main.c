8ba0217436b24f11414f8cc1918fe227abbf1df2
challenge_zzq_firstpush <812014285@qq.com>
2023-09-20 20:58:03 +0800

    汉诺塔递归搞明白了，刚学习会深度优先算法，后面再写

--- class/none/main.c
+++ class/none/main.c
@@ -3,10 +3,23 @@
 
 int main(){
 
-    printf("hello world!");
-    char key[]="zhouzhengqi";
-    int  b = strlen(key);
-    printf("%llu ", sizeof(key));
-    printf("%d ",b);
+//    printf("hello world!");
+//    char key[]="zhouzhengqi";
+//    int  b = strlen(key);
+//    printf("%llu ", sizeof(key));
+//    printf("%d ",b);
+//
+//
+
+    typedef int int10[10];
+    int10 b[20];
+    int c[20][10];
+    b[10][5]=1;
+    printf("%d ",b[10][5]);
+
+
+
+
+
     return 0;
 }
--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+//一次移动
+void move(char A,char B){
+    printf("%c -> %c\n",A,B);
+}
+//把A通过B移动到C
+void hano(int n,char A,char B,char C){
+    if(n==1){
+        move(A,C);
+    }
+    else{
+        hano(n-1,A,C,B);
+        move(A,C);
+        hano(n-1,B,A,C);
+    }
+}
+//这玩意自己写完都不知道他怎么能跑的
+
 int main(){
-    printf("刚在学习");
+    int n = 2;
+    hano(n,'A','B','C');
     return  0;
 }
