b90f2d80757292050258534ab769e85531cffbd2
caosihan <3430890052@qq.com>
2023-10-02 18:13:09 +0800

    p01

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,31 @@
-#include <printf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <unistd.h>
 
-int main() {
-    printf("hello world!\n");
-    return 0;
+
+void Running_letter(){
+    int i, j, f, k;
+    for (i = 1; i <= 140; i ++) {
+        printf("%c", 'A');
+        usleep(4800);
+        system("cls");
+        for (j = 0; j < i; j ++){
+            printf(" ");
+        }
+        j --;
+    }
+    for (f = 1; f <= 140; f ++) {
+        printf("%c", 'A');
+        usleep(4800);
+        system("cls");
+        for (k = 79; k > f; k --){
+            printf(" ");
+        }
+        k ++;
+    }
+}
+
+int main(){
+    Running_letter();
 }
