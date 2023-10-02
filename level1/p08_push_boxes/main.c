5275a449385306cbb3d4ef68bcc3d590e2f8b8d0
caosihan <3430890052@qq.com>
2023-10-02 18:18:11 +0800

    p08 推箱子

--- level1/p08_push_boxes/main.c
+++ level1/p08_push_boxes/main.c
@@ -1,6 +1,24 @@
-#include <printf.h>
+#include <stdio.h>
 
 int main() {
-    printf("hello world!\n");
+    int i,j,m,cnt=0;
+    int a[]={1,4,32,16,3,8,19,22,135,7,234,56,36,75,67,64,85,23,67,3,4,65,37,563,43,90};
+    int len=sizeof(a)/sizeof(a[0]);
+    for(i=0;i<len-1;i++){
+        for(j=i+1;j>0;j--){
+            if(a[j]<a[j-1]){
+                m=a[j];
+                a[j]=a[j-1];
+                a[j-1]=m;
+                cnt+=3;
+            }else{
+                cnt++;
+                continue;
+            }
+        }
+    }
+    for(i=0;i<len;i++)
+        printf("%d ",a[i]);
+    printf("次数=%d",cnt);
     return 0;
 }
