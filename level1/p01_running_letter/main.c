b4ec872a9781181fb3c00c8c9d45c70f9ba04e7e
Jiang Haoyu <144139731+12ded21@users.noreply.github.com>
2023-10-30 16:41:45 +0800

    push不上去哭了

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include<windows.h>
 #include<stdlib.h>
-#include<conio.h>
 int main() {
+    CONSOLE_CURSOR_INFO mice;
+    mice.dwSize=1;
+    mice.bVisible=FALSE;
+    HANDLE handle= GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorInfo(handle,&mice);
     char * letter="Hello";
     int m=0;
     int k=1;
@@ -12,8 +16,12 @@ int main() {
         if(m==57)
             k=-1;
         for(int i=1;i<=m*2;i++)
+        {
             printf(" ");
+            fflush(stdout);
+        }
         printf("%s",letter);
+        fflush(stdout);
         Sleep(300);
         if(k==1)
             m++;
