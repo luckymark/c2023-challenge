1b53f7daae11529d3af7fc22f39b76568571d76f
challenge_zzq_firstpush <812014285@qq.com>
2023-10-31 13:51:05 +0800

    完成 level1_p01

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,40 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <windows.h>
 
 #define SCREEN_WIDTH 40
+
+//更好用的清屏
 void ClearConsole()
 {
-    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    COORD home = {0, 0};
-    DWORD written;
-    FillConsoleOutputCharacter(hOut, ' ', 80 * 25, home, &written);
-    FillConsoleOutputAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE, 80 * 25, home, &written);
-    SetConsoleCursorPosition(hOut, home);
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD home = {0, 0};
+	DWORD written;
+	FillConsoleOutputCharacter(hOut, ' ', 80 * 25, home, &written);
+	FillConsoleOutputAttribute(hOut, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE, 80 * 25, home, &written);
+	SetConsoleCursorPosition(hOut, home);
 }
-int main() {
-    int position = 0;
-    int direction = 1;
 
-    char word[] = "Uestc.ZhouZhengqi";
-    int lenth = strlen(word);
 
-    while (1) {
-        ClearConsole();
-        //位置移动
-        for (int i = 0; i < position; i++) {
-            printf(" ");
-        }
-        printf("%s\n", word);
-        position += direction;
-        //判断方向
-        if (position >= SCREEN_WIDTH - lenth + 1 || position <= 0) {
-            direction *= -1;
-        }
+int main()
+{
+	int position = 0;
+	int direction = 1;
 
-        usleep(100000); // 控制运动速度，单位为微秒
-    }
+	char word[] = "Uestc.ZhouZhengqi";
+	int lenth = strlen(word);
+
+	while (1)
+	{
+		//更好用的清屏
+		ClearConsole();
+		//位置移动
+		for (int i = 0; i < position; ++i)
+		{
+			printf(" ");
+		}
+		printf("%s\n", word);
+		position += direction;
+		//判断方向
+		if (position > SCREEN_WIDTH - lenth || position < 0)
+		{
+			direction *= -1;
+			position += direction;
+		}
+
+		usleep(100000); // 控制运动速度，单位为微秒
+	}
 
 }
