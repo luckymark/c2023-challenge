3e4d3c41ed12ba6f8757877db217426c53924896
温馨 <3074463749@qq.com>
2023-11-04 15:12:30 +0800

    提交了P07

---
+++ level1/p07_maze/P07.c
@@ -0,0 +1,131 @@
+#include <conio.h>
+#include <stdio.h>
+#include <windows.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#define WIDTH 13
+#define LENGTH 42
+
+char map[WIDTH][LENGTH] = {
+        "+---+---+---+---+---+---+---+---+---+---+",
+        "|           |               |           |",
+        "+   +---+   +---+   +---+   +---+   +   +",
+        "|   |       |       |           |   |   |",
+        "+   +   +   +---+   +---+   +   +   +   +",
+        "|       |               |   |   |   |   |",
+        "+---+   +---+   +   +---+   +   +   +   +",
+        "|       |       |   |       |   |   |   |",
+        "+   +---+   +   +   +   +---+   +   +   +",
+        "|   |       |   |   |   |               |",
+        "+   +   +---+   +   +   +---+   +---+   +",
+        "        |       |   |   |       |       |",
+        "+---+---+---+---+---+---+---+---+---+   +"
+};
+
+void printMap();
+void initializeCharacter();
+void moveCharacter(short up, short down, short left, short right);
+COORD* getCursorPos();
+bool isExit();
+
+
+int main() {
+    system("chcp 65001");
+    //打印迷宫地图
+    printMap();
+
+    //生成角色
+    initializeCharacter();
+
+    //读取光标并移动角色
+    move:
+    switch (getch()) {
+        case 80://键盘输入的向上箭头ASCII码值为80
+            moveCharacter(1, 0, 0, 0);
+            break;
+        case 72://键盘输入的向下箭头ASCII码值为72
+            moveCharacter(0, 1, 0, 0);
+            break;
+        case 75://键盘输入的向左箭头ASCII码值为75
+            moveCharacter(0, 0, 1, 0);
+            break;
+        case 77://键盘输入的向右箭头ASCII码值为77
+            moveCharacter(0, 0, 0, 1);
+            break;
+    }
+
+    //判断玩家是否到达出口，如果成功到达，则打印恭喜信息；如果未到达，则通过goto语句回到switch继续操作
+    if (isExit()) {
+        system("cls");
+        printf("%s", "恭喜！！你已通过该迷宫！！！");
+        system("pause");
+    }
+    else {
+        goto move;
+    }
+}
+
+
+void printMap() {//双层循环打印地图
+    for (int i = 0; i < WIDTH; ++ i) {
+        for (int j = 0; j < LENGTH; ++j) {
+            printf("%c", map[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+COORD* getCursorPos(){
+    COORD* coord=(COORD*)malloc(sizeof(COORD));
+    CONSOLE_SCREEN_BUFFER_INFO cursor_info;
+    HANDLE hOutput= GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOutput == INVALID_HANDLE_VALUE || !GetConsoleScreenBufferInfo(hOutput, &cursor_info)){
+        printf("%s", "获取光标当前位置失败，请重试！");
+        exit(-1);
+    }
+    else {
+        (*coord).X=cursor_info.dwCursorPosition.X;
+        (*coord).Y=cursor_info.dwCursorPosition.Y;
+    }//获取光标当前位置，存储到coord中
+
+    return coord;
+}
+
+void initializeCharacter(){
+    COORD *position=(COORD*)malloc(sizeof(COORD));
+    (*position).X = 1;
+    (*position).Y = 11;//定义一个COORD类型的变量，并将其位置定在迷宫入口处
+
+    HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);//获取控制台的标准输出句柄
+    SetConsoleCursorPosition(hOutput, *position);//将光标移动到position指定的位置
+
+    printf("%c", 'O');//将人物形象设置为大写字母O
+}
+
+void moveCharacter(short up, short down, short left, short right){
+    HANDLE hOutput= GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD* coord=getCursorPos();
+
+    printf("\b\x20\b"); //打印空格清除掉原本的角色
+    (*coord).X -= 1;
+
+    //根据输入修改coord的值
+    (*coord).X += right;
+    (*coord).X -= left;
+    (*coord).Y += up;
+    (*coord).Y -= down;
+
+    SetConsoleCursorPosition(hOutput, *coord);//将光标移动到coord指定的位置
+    printf("%c", 'O');//在移动后的指定位置打印人物
+}
+
+
+bool isExit(){
+    COORD* coord=getCursorPos();
+    if ((*coord).X >= 38 && (*coord).Y == 13) {//如果到达了地图出口的坐标位置，返回true；如果没有，返回false
+        return true;
+    }
+    else
+        return false;
+}
