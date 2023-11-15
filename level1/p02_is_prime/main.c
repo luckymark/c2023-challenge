700d41048dfebe324cf98fbe52d97d95497d9298
challenge_zzq_firstpush <812014285@qq.com>
2023-11-15 08:27:13 +0800

    level1_p02完成，优化了代码进行了备注

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
-int main() {
-    system("chcp 65001");           //输出中文
-    int n;
-    int a=0;                        //定义一个变量用来判断是否是素数
-    printf("请输入一个数字n:\n");
-    scanf("%d",&n);
-    float b= pow(n,0.5);      //定义中间变量减少运算次数
-    //判断素数
-    for (int i = 2; i < b; i++) {
-        if(n%i==0)
-        {
-            a=0;
-            printf("这个数不是素数。");
-            break;
-        }
-
-    }
-    if(a == 1)
-        printf("这个数是素数。");
-    return 0;
+int main()
+{
+	//输出中文
+	system("chcp 65001");
+	int n;
+	printf("请输入一个数字n:\n");
+	scanf("%d", &n);
+	//计算中间量减少运算次数
+	float b = pow(n, 0.5);
+	//判断素数
+	for (int i = 2; i < b; i++)
+	{
+		if (n % i == 0)
+		{
+			printf("这个数不是素数。");
+			return 0;
+		}
+	}
+	//所有的数都不能整除，那么这个数就是素数
+	printf("这个数是素数。");
+	return 0;
 }
