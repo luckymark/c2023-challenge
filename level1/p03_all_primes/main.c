0bed454e67939457fa334aa651929b8ef11e271f
challenge_zzq_firstpush <812014285@qq.com>
2023-11-15 15:32:03 +0800

    level1_p03完成，优化了代码进行了备注

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -3,32 +3,35 @@
 #include <math.h>
 #include <windows.h>
 
-int main() {
-    system("chcp 65001");//输出中文
-    int w = 1;
-    clock_t start,end;//定义时间计算的变量
-    double times;
-    printf("2 ");
-    start = clock();            //计时开始
-    //判断是否是素数
-    for (int n = 3; n < 1001; n++) {
-        w = 1;
-        //定义中间变量减少运算次数
-        double b= pow(n,0.5);
-        for (int i = 2; i <= b; i++) {
-            if(n%i==0)
-            {
-                w=0;
-                break;
-            }
-
-        }
-        if(w==1)
-            printf("%d ",n);
-    }
-    end = clock();
-    printf("\n");
-    times = (double)(end - start)/CLOCKS_PER_SEC;
-    printf("计算时间为：%f 秒\n",times);
-    return 0;
+int main()
+{
+	system("chcp 65001");
+	//定义素数判断的变量
+	int prime;
+	//定义时间计算的变量
+	clock_t start, end;
+	printf("2 ");
+	//计时开始
+	start = clock();
+	//判断3-1000之间的某些数字是否是素数
+	for (int n = 3; n < 1001; n++)
+	{
+		prime = 1;
+		//定义中间变量减少运算次数
+		double b = pow(n, 0.5);
+		for (int i = 2; i <= b; i++)
+		{
+			if (n % i == 0)
+			{
+				prime = 0;
+				break;
+			}
+		}
+		if (prime == 1)
+			printf("%d ", n);
+	}
+	end = clock();
+	printf("\n");
+	printf("计算时间为：%f 秒\n", (double)(end - start) / CLOCKS_PER_SEC);
+	return 0;
 }
