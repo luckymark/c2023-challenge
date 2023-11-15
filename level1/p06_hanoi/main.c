33b97807d05a50c35439e595049747c00503e104
challenge_zzq_firstpush <812014285@qq.com>
2023-11-15 15:34:56 +0800

    level1_p05完成，优化了代码进行了备注（虽然基本没怎么变化）

--- level1/p06_hanoi/main.c
+++ level1/p06_hanoi/main.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 //一次移动
-void move(char A,char B){
-    printf("%c -> %c\n",A,B);
+void move(char A, char B)
+{
+	printf("%c -> %c\n", A, B);
 }
 //把A通过B移动到C
-void hano(int n,char A,char B,char C){
-    if(n==1){
-        move(A,C);
-    }
-    else{
-        hano(n-1,A,C,B);
-        move(A,C);
-        hano(n-1,B,A,C);
-    }
+void hano(int n, char A, char B, char C)
+{
+	if (n == 1)
+	{
+		move(A, C);
+	}
+	else
+	{
+		hano(n - 1, A, C, B);
+		move(A, C);
+		hano(n - 1, B, A, C);
+	}
 }
-//这玩意自己写完都不知道他怎么能跑的
 
-int main(){
-    int n = 3;
-    hano(n,'A','B','C');
-    return  0;
+int main()
+{
+	//输入盘子的个数
+	printf("请输入盘子的个数：");
+	int n;
+	scanf("%d", &n);
+	hano(n, 'A', 'B', 'C');
+	return 0;
 }
