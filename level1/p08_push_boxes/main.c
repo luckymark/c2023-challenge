84a4544a4085cc9914a8cba280ea69b94647c2ee
Cao Sihan <144092344+MagicVicCoder@users.noreply.github.com>
2023-12-28 13:28:57 +0800

    Update main.c

--- level1/p08_push_boxes/main.c
+++ level1/p08_push_boxes/main.c
@@ -1,24 +1,82 @@
-#include <stdio.h>
-
-int main() {
-    int i,j,m,cnt=0;
-    int a[]={1,4,32,16,3,8,19,22,135,7,234,56,36,75,67,64,85,23,67,3,4,65,37,563,43,90};
-    int len=sizeof(a)/sizeof(a[0]);
-    for(i=0;i<len-1;i++){
-        for(j=i+1;j>0;j--){
-            if(a[j]<a[j-1]){
-                m=a[j];
-                a[j]=a[j-1];
-                a[j-1]=m;
-                cnt+=3;
-            }else{
-                cnt++;
-                continue;
-            }
-        }
+#include<stdio.h>
+#include<stdlib.h>
+#include<Windows.h>
+#include<conio.h>
+int main(void)
+{
+    char map[50][50]={
+                        "######",
+                        "#O #  ",
+                        "# ## #",
+                        "#  # #",
+                        "##   #",
+                        "######",
+                        };
+    int i,x,y,p,q;
+    int ch;
+    x=1;y=1;p=1;q=5;
+    printf("\t\t\t\t迷宫游戏\n");
+    printf("操作方式:\n\tw为往上走\n\ts为往下走\n\ta为往左走\n\td为往右走\n");
+   	printf("\n\n");
+    for (i=0;i<6;i++){
+    	printf("\t\t\t\t");
+        puts(map[i]);
     }
-    for(i=0;i<len;i++)
-        printf("%d ",a[i]);
-    printf("次数=%d",cnt);
+    while(x!=p||y!=q)
+    {
+        ch=getch();
+        switch(ch)
+        {
+        	case 's':
+            {
+                if(map[x+1][y]!='#')
+                {                       
+                    map[x][y]=' ';
+                    x++;
+                    map[x][y]='O';
+                }
+            }
+            break;
+            case 'w':
+            {
+                if(map[x-1][y]!='#')
+                {                       
+                    map[x][y]=' ';
+                    x--;
+                    map[x][y]='O';
+                }
+            }
+            break;
+            case 'a':
+            {
+                if(map[x][y-1]!='#')
+                {                       
+                    map[x][y]=' ';
+                    y--;
+                    map[x][y]='O';
+                }
+            }
+            break;
+            case 'd':
+            {
+                if(map[x][y+1]!='#')
+                {                       
+                    map[x][y]=' ';
+                    y++;
+                    map[x][y]='O';
+                }
+            }
+		}
+        system("cls");
+        printf("\t\t\t\t迷宫游戏\n");
+	    printf("操作方式:\n\tw为往上走\n\ts为往下走\n\ta为往左走\n\td为往右走\n");
+	    printf("\n\n");
+	    for (i=0;i<6;i++){
+    		printf("\t\t\t\t");
+        	puts(map[i]);
+    	}
+    }
+    system("cls");
+    printf("恭喜你，成功了！");
     return 0;
 }
